8094170f2f1717bf1125989302a5df8264b12cec
gerardodev25 <gerardodev25@gmail.com>
2021-05-27 21:40:48 -0400

    active mobile finish

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const share = document.getElementById("share");
+const share2 = document.getElementById("share-2");
+const card = document.getElementById("card");
 
 share.addEventListener("click", (e) => {
    e.stopPropagation();
-   console.log(e.target);
+   // console.log(e.target);
+   card.classList.add("no-active");
+});
+
+share2.addEventListener("click", (e) => {
+   e.stopPropagation();
+   card.classList.remove("no-active");
+
 });
